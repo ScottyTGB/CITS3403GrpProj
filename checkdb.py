8c2f9226cace2eba067c50a9c2b1b28ca2e30773
tmeinertz <tmeinertz@outlook.com>
2024-05-09 15:18:10 +0800

    Some temporary request frameworks, need to work on pulling user data from userID

--- checkdb.py
+++ checkdb.py
@@ -1,7 +1,14 @@
 import sqlite3
 
+
 con_students = sqlite3.connect("user.db")  
 cur_students = con_students.cursor()        
 cur_students.execute("select * from user")   
 for row in cur_students.fetchall():
+  print([str(value) for value in row])
+
+con_requests = sqlite3.connect("request.db")  
+cur_requests = con_requests.cursor()        
+cur_requests.execute("select * from request")   
+for row in cur_requests.fetchall():
   print([str(value) for value in row])
