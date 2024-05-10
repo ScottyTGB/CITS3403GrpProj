e367171b490fa602d555b16322cf1d3cc2750772
ScottyTGB <gwegwe12@gmail.com>
2024-05-10 10:57:00 +0800

    Navbar updated
    Responsive navbar design implemented.
    Optimizations to be done.
    Offcanvas navbar to be populated.

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,3 +1,4 @@
+// Checks if the page is scrolled, and updates the navbar visuals accordingly
 window.addEventListener('scroll', function() {
   const nav = document.querySelector('nav');
   if (window.pageYOffset > 0) {
