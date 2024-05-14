a7bd4f40185bf2943df3af50bb0ae2e20b97a4b6
Scott <gwegwe12@gmail.com>
2024-05-14 14:32:25 +0800

    Index responsiveness fixed
    Service boxes spaced and made responsive on mobile devices

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ window.onload = function() {
   document.getElementById('readMore').addEventListener('click', function(event) {
     event.preventDefault();
     window.scrollBy({
-      top: window.innerHeight,
+      top: window.innerHeight / 1.5,
       left: 0,
       behavior: 'smooth'
     });
